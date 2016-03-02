e9ef11bef6de613ab64d256c6c00d4939f3130ed
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2016-03-01 22:14:09 -0500

    12 and 13

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh?")
+
+print("so you are %r old, and %r tall and weigh %r lbs" %(age, height, weight))
---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("The first variable is: ", first)
+print("The second variable is :", second)
+print("The third variable is :", third)
+
+
+random = input("Now enter anything you want here : ")
+
+print(" third was %r : and random was %r" %(third, random))
